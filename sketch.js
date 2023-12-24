07f35e125242f601150ba881ab31ba5f2a86c1ea
vn126 <125955617+vn126@users.noreply.github.com>
2023-12-24 16:19:11 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,28 +1,14 @@
-var granularity;
-
 function setup() {
     // put setup code here
-    createCanvas(400, 400);
-    rectMode(CENTER);
-    fill('rgba(100, 255, 255, 10)');
-    stroke('rgb(250, 100, 100)');
-    max_depth = 5;
-    granularity = width/(4*max_depth);
+    createCanvas(400, 300);
+    background(0);
 }
-
 function draw() {
     // put drawing code here
-    background(255);
-    len = width/2;
-    drawSquare(width/2, height/2, len, granularity);
-}
-
-function drawSquare(x, y, len){
-    square(x, y, len);
-    if (len > granularity) {
-        drawSquare(x, y-len/2, len/2);
-        drawSquare(x+len/2, y, len/2);
-        drawSquare(x, y+len/2, len/2);
-        drawSquare(x-len/2, y, len/2);
-    }
+    stroke(255);
+    s = random(1, 10);
+    x = random(0, width);
+    y = random(0, height);
+    strokeWeight(s);
+    point(x, y);
 }
